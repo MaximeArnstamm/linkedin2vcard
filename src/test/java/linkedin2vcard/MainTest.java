7263656571d2340ef0d71c17ed6686400db855d2
Maxime Arnstamm <Maxime@Maxime.Maxime>
2016-10-10 16:26:37 +0200

    noobie mistake : uploaded now revoked private api key

--- src/test/java/linkedin2vcard/MainTest.java
+++ src/test/java/linkedin2vcard/MainTest.java
@@ -7,7 +7,7 @@ public class MainTest {
 	//@Test
 	public void run() {
 		try {
-			Main.run("https://www.linkedin.com/in/philippe-huriaux-04a15552", "c1a70e87cc4dc809e93e6fc90f3aca93ce401371");
+			Main.run("LINKEDIN URL", "API KEY");
 		}
 		catch (IOException e) {
 			e.printStackTrace();
